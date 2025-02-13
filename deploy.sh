78727a425517536a1fa32f1fc58c81d584d5c5f9
Daniel Dias <lupeke@gmail.com>
2025-02-13 20:02:33 -0300

    (fix) missing cmd

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+CLOUD=aws
+ZONE=sa-east-1
+
 go build -o gifkernel server.go
 
 ops pkg from-run --name gifkernel --version v0.0 gifkernel
 
-ops instance create gifkernel -c config.json -t aws -z sa-east-1 --show-debug
+ops image create -t $CLOUD -c config.json --package gifkernel_v0.0 -i gifkernel --local --show-debug
+
+ops instance create gifkernel -c config.json -t $CLOUD -z $ZONE --show-debug
